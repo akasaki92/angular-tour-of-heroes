f5bae09424b2032a12399dec0df3bc4990a4125f
Adam Santiaji <adam.wikrama@gmail.com>
2018-08-06 09:49:50 +0700

    added navigate hero-detail

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  
-  selectedHero: Hero;
+    
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -22,8 +21,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }  
+ 
 }
